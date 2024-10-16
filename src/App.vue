18cea6b78de18fb66ef4534717e3f25c71b2d0c7
CHOR NARIN <narintkh178@gmail.com>
2024-10-16 23:16:55 +0700

    TP1 Vue js project

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<template>
+  <div>
+    <header>
+      <div class="wrapper">
+        <h1>This is my first VueJs project</h1>
+      </div>
+    </header>
+
+    <RouterView />
+  </div>
+</template>
+
+<style scoped>
+  html, body {
+  height: 100%; 
+  margin: 0;
+  }
+
+.wrapper {
+  display: flex;
+  justify-content: center; 
+  align-items: center;   
+  height: 100vh;        
+}
+
+h1 {
+  font-size: 2rem;  
+}
+
+</style>
