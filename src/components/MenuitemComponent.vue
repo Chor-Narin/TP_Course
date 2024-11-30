82af1e5a9a53404d1c5cccbbf32972f6b685ea5c
CHOR NARIN <narintkh178@gmail.com>
2024-11-30 13:34:17 +0700

    TP4 exersices 1 and 2

---
+++ src/components/MenuitemComponent.vue
@@ -0,0 +1,27 @@
+<!-- <template>
+    <div class="container">
+      <select v-model="SelectItems" class="first-container">
+        <option value="" disabled selected>Select an item</option>
+        <option v-for="item in items" :key="item" :value="item">{{ item }}</option>
+      </select>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: 'MenuitemComponent',
+    data() {
+      return {
+        items: ['cabbage', 'tomato', 'potato', 'onion'], // List of items
+        SelectItems: '' // No default item selected
+      };
+    }
+  };
+  </script>
+  
+  <style scoped>
+  .first-container {
+    color: black;
+  }
+  </style>
+   -->
